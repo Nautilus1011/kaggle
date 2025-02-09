c83151df310f4d3b1eda565217c5ca252edc1e54
jin.ysd@hotmail.com <jin.ysd@hotmail.com>
2025-02-09 18:51:24 +0900

    add a folder and moved some file

---
+++ titanic/src/RandomForestComplition.py
@@ -0,0 +1,70 @@
+import numpy as np
+import pandas as pd
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set(style="darkgrid")
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder, StandardScaler
+from sklearn.metrics import roc_curve, auc
+from sklearn.model_selection import StratifiedKFold
+
+import string
+import warnings
+warnings.filterwarnings('ignore')
+
+SEED = 42
+
+
+
+def concat_df(train_data, test_data):
+    # Returns a concatenated df of training and test set
+    return pd.concat([train_data, test_data], sort=True).reset_index(drop=True)
+
+def divide_df(all_data):
+    # Returns divided dfs of training and test set
+    return all_data.loc[:890], all_data.loc[891:].drop(['Survived'], axis=1)
+
+df_train = pd.read_csv(r'E:\repository\kaggle\titanic\data\test.csv')
+df_test = pd.read_csv('../input/test.csv')
+df_all = concat_df(df_train, df_test)
+
+df_train.name = 'Training Set'
+df_test.name = 'Test Set'
+df_all.name = 'All Set' 
+
+dfs = [df_train, df_test]
+
+print('Number of Training Examples = {}'.format(df_train.shape[0]))
+print('Number of Test Examples = {}\n'.format(df_test.shape[0]))
+print('Training X Shape = {}'.format(df_train.shape))
+print('Training y Shape = {}\n'.format(df_train['Survived'].shape[0]))
+print('Test X Shape = {}'.format(df_test.shape))
+print('Test y Shape = {}\n'.format(df_test.shape[0]))
+print(df_train.columns)
+print(df_test.columns)
+
+
+
+print(df_train.info())
+df_train.sample(3)
+
+print(df_test.info())
+df_test.sample(3)
+
+
+# 欠損値を見つけ出す関数らしい
+def display_missing(df):    
+    for col in df.columns.tolist():          
+        print('{} column missing values: {}'.format(col, df[col].isnull().sum()))
+    print('\n')
+    
+for df in dfs:
+    print('{}'.format(df.name))
+    display_missing(df)
+    
+# ageの欠損値を中央値で埋めるのではなく、
+df_all_corr = df_all.corr().abs().unstack().sort_values(kind="quicksort", ascending=False).reset_index()
+df_all_corr.rename(columns={"level_0": "Feature 1", "level_1": "Feature 2", 0: 'Correlation Coefficient'}, inplace=True)
+df_all_corr[df_all_corr['Feature 1'] == 'Age']
